869d635d2fae52ad60e2278a7f8ba108fb0d4aac
Jesus Valadez <kzkz98@outlook.com>
2018-10-26 01:10:09 -0500

    Actualizacion de casos de prueba en el programa a ejecutar

--- TestProgram.asm
+++ TestProgram.asm
@@ -1,5 +1,23 @@
+.data 
+
+Vector: .word 0 0 0 0 0 0 0 0
 	
 .text
+#andi
+	addi $s0, $zero, 5
+	andi $t0, $s0, 3
+
+	lui $1, 0x00001001       
+	ori $s1, $1, 0x00000000 
+#sw	
+	#sw $s0, ($s1)
+	#sw $t0, -4($s1)
+#lw	
+	#lw $t1, ($s1)
+	#lw $t0, -4($s1)
+#j	
+	#j SALTO 
+	
 	lui $s0, 0x00000101
 	ori $s1, $s0, 0x24
 	addi $s2, $zero, 1
@@ -7,8 +25,9 @@
 	sll $t0, $s2, 4
 	srl $t1, $s3, 4
 	sub $t2, $t0, $t1
+	
+SALTO:
 
-	#instruccion andi
-	andi $s0, $s1, 0x25
+	addi $s0, $zero, 1
 	
 	
