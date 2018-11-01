59538640750242b73bef339c38d61705818cb238
Jesus Valadez <kzkz98@outlook.com>
2018-10-31 19:54:16 -0600

    Actualizacion de simulacion y programa para probar instruccion lw y sw

--- TestProgram.asm
+++ TestProgram.asm
@@ -4,11 +4,20 @@ Vector: .word 0 0 0 0 0 0 0 0
 	
 .text
 #andi
-	addi $s0, $zero, 5
-	andi $t0, $s0, 3
+	#addi $s0, $zero, 5
+	#andi $t0, $s0, 3
 
 	lui $1, 0x00001001       
-	ori $s1, $1, 0x00000000 
+	ori $s1, $1, 0x00000000
+	
+	addi $t0, $zero, 2
+	addi $t1, $zero, 4
+	
+	sw $t0, ($s1)
+	sw $t1, 4($s1)
+	
+	lw $t2, 4($s1)
+	
 #sw	
 	#sw $s0, ($s1)
 	#sw $t0, -4($s1)
@@ -18,16 +27,16 @@ Vector: .word 0 0 0 0 0 0 0 0
 #j	
 	#j SALTO 
 	
-	lui $s0, 0x00000101
-	ori $s1, $s0, 0x24
-	addi $s2, $zero, 1
-	addi $s3, $zero, 32
-	sll $t0, $s2, 4
-	srl $t1, $s3, 4
-	sub $t2, $t0, $t1
+#	lui $s0, 0x00000101
+#	ori $s1, $s0, 0x24
+#	addi $s2, $zero, 1
+#	addi $s3, $zero, 32
+#	sll $t0, $s2, 4
+#	srl $t1, $s3, 4
+#	sub $t2, $t0, $t1
 	
 SALTO:
 
-	addi $s0, $zero, 1
+#	addi $s0, $zero, 1
 	
 	
