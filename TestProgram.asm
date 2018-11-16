a8612b4688008ffa4d58f9b10b07499d2773bc15
Jesus Valadez <kzkz98@outlook.com>
2018-11-16 05:46:28 -0600

    Programa a ejecutar modificado y compilado. Se agregaron 3 nop entre todas las instrucciones

--- TestProgram.asm
+++ TestProgram.asm
@@ -7,15 +7,51 @@
 .text
 	nop
  	addi $t0, $zero, 5
+ 	nop
+ 	nop
+ 	nop
  	add  $t1, $t0,   $zero
+ 	nop
+ 	nop
+ 	nop
   	addi $t1, $t1,   2
+  	nop
+ 	nop
+ 	nop
  	addi $t2, $t1,   3
+ 	nop
+ 	nop
+ 	nop
   	addi $t3, $t3,   0x010010000
+  	nop
+ 	nop
+ 	nop
  	sw   $t2, 0($t3)
-  
+ 	nop
+ 	nop
+ 	nop
   	add  $s0, $t2,   $t1
+  	nop
+ 	nop
+ 	nop
+ 	nop
  	sub  $s1, $s0,   $t3
+ 	nop
+ 	nop
+ 	nop
  	lw   $t2, 0($t3)
+ 	nop
+ 	nop
+ 	nop
  	addi $s2, $s2,   -2
+ 	nop
+ 	nop
+ 	nop
  	or   $s2, $s2,   $t4
+ 	nop
+ 	nop
+ 	nop
   	sll  $s7, $s2,   2
+  	nop
+ 	nop
+ 	nop
